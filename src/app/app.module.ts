bf5a077cd118dd9922186e55778374d338078075
Paulo Roque <paulinhoroque@hotmail.com>
2022-03-09 00:31:12 -0300

    Criando moludo dos components material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from './material/material.module';
 
 
 
@@ -36,15 +37,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     FlexLayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatInputModule,
-    MatCardModule,
-    MatTableModule,
-    MatListModule,
-    MatTooltipModule,
+    MaterialModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
